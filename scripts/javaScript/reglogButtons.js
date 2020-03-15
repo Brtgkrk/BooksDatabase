b913ef90926db1d43790587de056b444289d107a
Jakub Krok <jakubkrok2000@interia.pl>
2020-03-15 10:54:38 +0100

    New main page look
    Changed colors and index page look.

--- scripts/javaScript/reglogButtons.js
+++ scripts/javaScript/reglogButtons.js
@@ -1,27 +1,26 @@
 //Login
 
-$( "#login-button-canvas" ).click(function() {  //login click
-    formAnimation('-60%', '0', '#login-form', '30%', '1');
+$("#btn-login").click(function () {  //login click
+    formAnimation('10%', '1', '#login-form', '-30%', '0');
 });
 
-$( "#login-quit" ).click(function() {   //login quit
+$("#login-quit").click(function () {   //login quit
     formAnimation('40%', '1', '#login-form', '-30%', '0');
 });
 
 //Register
 
-$( "#register-button-canvas" ).click(function() {
-    formAnimation('-60%', '0', '#register-form', '30%', '1');
+$("#btn-register").click(function () {
+    formAnimation('60%', '1', '#register-form', '-30%', '0');
 });
 
-$( "#register-quit" ).click(function() {
+$("#register-quit").click(function () {
     formAnimation('40%', '1', '#register-form', '-30%', '0');
 });
 
 //Functions
 
-function formAnimation(topButtons, opacityButtons, elem, topElem, opacityElem)
-{
+function formAnimation(topButtons, opacityButtons, elem, topElem, opacityElem) {
     $('#register-button-canvas').animate({
         top: topButtons,
         opacity: opacityButtons
